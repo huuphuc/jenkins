1526edecd8b63b7429f71bc484c46bb16788da14
huuphuc <huuphuc@jv-it.com.vn>
2013-01-19 02:48:29 +0700

    Aaaa

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<?php
+
+	class helloworld {
+		private $tmp;
+		public function hello(){
+			$i = 8;
+			$hello = 'aaa';
+			echo "hello world";
+		}
+
+		public function goodbye($name, $du){
+			echo "goodbye".$name;
+		}
+
+		private function kakaka(){
+			return "Aaaa";
+		}
+
+	}
