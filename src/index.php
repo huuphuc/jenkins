854be6fa2e96d372c02faa5d7d977ed51c8227f4
huuphuc <huuphuc@jv-it.com.vn>
2013-01-18 21:57:13 +0700

    update code with error

--- src/index.php
+++ src/index.php
@@ -1,13 +1,19 @@
 <?php
 
 	class helloworld {
-
+		private $tmp;
 		public function hello(){
+			$i = 8;
+			$hello = 'aaa';
 			echo "hello world";
 		}
 
-		public function goodbye(){
-			echo "goodbye";
+		public function goodbye($name, $du){
+			echo "goodbye".$name;
+		}
+
+		private function kakaka(){
+			return "Aaaa";
 		}
 
 	}
