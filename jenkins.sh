1acb7ae587eeb434204bd68bc61a598e3e82ee54
huuphuc <huuphuc@jv-it.com.vn>
2013-01-25 10:09:37 +0700

    aaaaaaaaaaa

---
+++ jenkins.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+HOME_BUILD = "/data/webroot/jenkins";
+RESULT = $HOME_BUILD;
+
+if [ ! -d "{$HOME_BUILD}/build" ]; then
+    mkdir $RESULT/build
+    mkdir $RESULT/build/logs
+fi
+
+rm -rf $RESULT/build/logs/*.xml
+
+/data/binary/php/bin/phpcpd --log-pmd $RESULT/build/logs/cpd.xml html/
+
+/data/binary/php/bin/phpmd html/ xml unusedcode,codesize > $RESULT/build/logs/pmd.xml
